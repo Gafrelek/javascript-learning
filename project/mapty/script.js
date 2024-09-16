76629fb163060d1667b5660b5f3e1d3de2de32d7
Gregor F <gafrelek@gmail.com>
2024-09-16 23:00:48 +0200

    created Running and Cyclic classes and their methods

--- project/mapty/script.js
+++ project/mapty/script.js
@@ -11,6 +11,47 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevation) {
+    super(coords, distance, duration);
+    this.elevation = elevation;
+  }
+
+  calcSpeed() {
+    // km/h
+    this.pace = this.distance / (this.duration / 60);
+    return this.pace;
+  }
+}
+
+const run1 = new Runnin([39, -12], 5.2, 24, 178);
+const run1 = new Cyclinc([39, -12], 27, 24, 178);
+/////////////////////////////
 class App {
   #map;
   #mapEvent;
