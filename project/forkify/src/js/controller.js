23ea834746bc06db8456121fa584318ff8d60a52
Gafrelek <gafrelek@gmail.com>
2024-09-29 19:51:53 +0200

    MVC implemented

--- project/forkify/src/js/controller.js
+++ project/forkify/src/js/controller.js
@@ -1,9 +1,8 @@
-import icons from 'url:../img/icons.svg';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 import 'core-js';
 import 'regenerator-runtime';
-
-const recipeContainer = document.querySelector('.recipe');
-const searchBtn = document.querySelector('.search__btn');
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -16,160 +15,26 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-// async function getRecipies(keyword) {
-// TODO move API key to ENV
-//   const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${keyword}&key=76bd912e-147e-4518-801e-a22651743399`;
-//   try {
-//     const response = await fetch(url);
 
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-
-//     const json = await response.json();
-//     console.log(json);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-const renderSpiner = function (parentEl) {
-  const markup = `<div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div>`;
-
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-};
-
-async function showRecipe() {
-  // 1. Loading recipe
-  renderSpiner(recipeContainer);
-  // TODO move API key to ENV
-  const url = `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`;
+async function controlRecipes() {
   try {
-    const response = await fetch(url);
+    // TODO move API key to ENV
+    const id = window.location.hash.slice(1);
+    if (!id) return;
 
-    if (!response.ok) throw new Error(`Response status: ${response.status}`);
+    recipeView.renderSpiner();
 
-    console.log(response);
+    // 1. Loading recipe
+    await model.loadRecipe(id);
 
-    const json = await response.json();
-    let { recipe } = json.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-
-    // 2. Showing recipe
-
-    const markup = `
-      <figure class="recipe__fig">
-        <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
-        <h1 class="recipe__title">
-          <span>${recipe.title}</span>
-        </h1>
-      </figure>
-
-      <div class="recipe__details">
-        <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use href="${icons}#icon-clock"></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--minutes">${
-            recipe.cookingTime
-          }</span>
-          <span class="recipe__info-text">minutes</span>
-        </div>
-        <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use href="${icons}#icon-users"></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--people">${
-            recipe.servings
-          }</span>
-          <span class="recipe__info-text">servings</span>
-
-          <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-minus-circle"></use>
-              </svg>
-            </button>
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-plus-circle"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
-
-        <div class="recipe__user-generated">
-          <svg>
-            <use href="${icons}#icon-user"></use>
-          </svg>
-        </div>
-        <button class="btn--round">
-          <svg class="">
-            <use href="${icons}#icon-bookmark-fill"></use>
-          </svg>
-        </button>
-      </div>
-
-      <div class="recipe__ingredients">
-        <h2 class="heading--2">Recipe ingredients</h2>
-        <ul class="recipe__ingredient-list">
-        ${recipe.ingredients
-          .map(ing => {
-            return `<li class="recipe__ingredient">
-            <svg class="recipe__icon">
-              <use href="${icons}#icon-check"></use>
-            </svg>
-            <div class="recipe__quantity">${ing.quantity ?? ''}</div>
-            <div class="recipe__description">
-              <span class="recipe__unit">${ing.unit}</span>
-              ${ing.description}
-            </div>
-          </li>`;
-          })
-          .join('')}
-        </ul>
-      </div>
-
-      <div class="recipe__directions">
-        <h2 class="heading--2">How to cook it</h2>
-        <p class="recipe__directions-text">
-          This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${
-            recipe.publisher
-          }</span>. Please check out
-          directions at their website.
-        </p>
-        <a
-          class="btn--small recipe__btn"
-          href="${recipe.sourceUrl}"
-          target="_blank"
-        >
-          <span>Directions</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </div>`;
-
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    // 2. Rendering recipe
+    recipeView.render(model.state.recipe);
   } catch (error) {
     console.error(error.message);
   }
 }
 
-showRecipe();
+controlRecipes();
+const events = ['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
---
+++ project/forkify/src/js/model.js
@@ -0,0 +1,31 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+
+    if (!response.ok) throw new Error(`Response status: ${response.status}`);
+
+    const json = await response.json();
+
+    const { recipe } = json.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(state.recipe);
+  } catch (error) {
+    console.error(error);
+  }
+};
